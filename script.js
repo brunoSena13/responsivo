c1cfa5076ff4417363376ed953c72a5b0d7b58cd
Bruno Sena <103772922+brunoSena13@users.noreply.github.com>
2022-04-23 14:41:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const btn = document.querySelector("#btn");
+
+function ativarMenu(){
+    const nav = document.querySelector("nav");
+    nav.classList.toggle('active');
+    if(nav.className == 'active'){
+        document.querySelector('.icon').src = "img/close.svg";
+    }else{
+        document.querySelector('.icon').src = "img/menu.svg";
+    }
+}
+
+btn.addEventListener("click", ativarMenu);
